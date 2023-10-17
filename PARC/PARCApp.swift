f18ec69d41d1f1f02605c0295543a691fe38bf9f
aym183 <ayman.ali1302@gmail.com>
2023-10-17 18:13:43 +0100

    Initial Commit

---
+++ PARC/PARCApp.swift
@@ -0,0 +1,17 @@
+//
+//  PARCApp.swift
+//  PARC
+//
+//  Created by Ayman Ali on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PARCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
