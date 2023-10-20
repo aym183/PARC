5fdb9c579fd85681b981ba678c3a8ea654d68fc1
aym183 <ayman.ali1302@gmail.com>
2023-10-20 09:17:10 +0100

    Added variable sizing in account and secondary market pages

---
+++ PARC/View/User/UserOpportunity.swift
@@ -0,0 +1,18 @@
+//
+//  UserOpportunity.swift
+//  PARC
+//
+//  Created by Ayman Ali on 20/10/2023.
+//
+
+import SwiftUI
+
+struct UserOpportunity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserOpportunity()
+}
