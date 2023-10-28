6641f9c2c48108404899062dd7745f90f3d3df1b
aym183 <ayman.ali1302@gmail.com>
2023-10-28 06:35:07 +0100

    Added list shares page

---
+++ PARC/View/User/UserListShares.swift
@@ -0,0 +1,18 @@
+//
+//  UserListShares.swift
+//  PARC
+//
+//  Created by Ayman Ali on 28/10/2023.
+//
+
+import SwiftUI
+
+struct UserListShares: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserListShares()
+}
