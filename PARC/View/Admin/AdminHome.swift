5b4aff59c0fdaca5a2ad2bfddc441a02c14d16e2
aym183 <ayman.ali1302@gmail.com>
2023-10-28 14:55:28 +0100

    Added Home page for admin

---
+++ PARC/View/Admin/AdminHome.swift
@@ -0,0 +1,18 @@
+//
+//  AdminHome.swift
+//  PARC
+//
+//  Created by Ayman Ali on 28/10/2023.
+//
+
+import SwiftUI
+
+struct AdminHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdminHome()
+}
