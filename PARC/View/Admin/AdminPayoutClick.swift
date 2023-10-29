d5a38da46df061c5ed8d8c92e3e91bc81bab6bee
aym183 <ayman.ali1302@gmail.com>
2023-10-29 06:24:25 +0000

    Added trading and payout click pages

---
+++ PARC/View/Admin/AdminPayoutClick.swift
@@ -0,0 +1,18 @@
+//
+//  AdminPayoutClick.swift
+//  PARC
+//
+//  Created by Ayman Ali on 29/10/2023.
+//
+
+import SwiftUI
+
+struct AdminPayoutClick: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdminPayoutClick()
+}
---
+++ PARC/View/Admin/AdminTradingClick.swift
@@ -0,0 +1,18 @@
+//
+//  AdminTradingClick.swift
+//  PARC
+//
+//  Created by Ayman Ali on 29/10/2023.
+//
+
+import SwiftUI
+
+struct AdminTradingClick: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdminTradingClick()
+}
