a553e45cb493ec3c1f96972907c3891fd33fed4b
aym183 <ayman.ali1302@gmail.com>
2023-11-07 09:44:14 +0000

    Added ImagePicker

---
+++ PARC/View/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  PARC
+//
+//  Created by Ayman Ali on 07/11/2023.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
