34e7cbb34e87e94c72f06ce5eb392da6b478ec3a
aym183 <ayman.ali1302@gmail.com>
2023-11-07 09:43:16 +0000

    Added AdminFranchiseForm

---
+++ PARC/View/Admin/AdminFranchiseForm.swift
@@ -0,0 +1,18 @@
+//
+//  AdminFranchiseForm.swift
+//  PARC
+//
+//  Created by Ayman Ali on 06/11/2023.
+//
+
+import SwiftUI
+
+struct AdminFranchiseForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdminFranchiseForm()
+}
