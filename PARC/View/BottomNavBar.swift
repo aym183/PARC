4a2a8a4616157bc29e1e8447c43110de06b2e045
aym183 <ayman.ali1302@gmail.com>
2024-01-31 11:47:38 +0000

    Added bottom navbar text

--- PARC/View/BottomNavBar.swift
+++ PARC/View/BottomNavBar.swift
@@ -18,30 +18,50 @@ struct BottomNavBar: View {
     private var fillImage: String {
         selectedTab.rawValue + ".fill"
     }
+    var tab_names: [String:String] = ["house": "Home", "chartPie": "Portfolio", "bag": "Secondary Market"]
     
     var body: some View {
         VStack {
             HStack {
                 ForEach(Tab.allCases, id: \.rawValue) { tab in
                     Spacer()
-                    Image(systemName: selectedTab == tab ? fillImage : tab.rawValue)
-                        .scaleEffect(selectedTab == tab ? 1.25 : 1.0)
-                        .foregroundColor(selectedTab == tab ? Color("Secondary") : .black)
-                        .font(.system(size: 22))
-                        .onTapGesture {
-                            withAnimation(.easeIn(duration: 0.1)) {
-                                selectedTab = tab
+                    VStack {
+                        Image(systemName: selectedTab == tab ? fillImage : tab.rawValue)
+                            .scaleEffect(selectedTab == tab ? 1.25 : 1.0)
+                            .foregroundColor(selectedTab == tab ? Color("Secondary") : .black)
+                            .font(.system(size: 22))
+                            .onTapGesture {
+                                withAnimation(.easeIn(duration: 0.1)) {
+                                    selectedTab = tab
+                                }
                             }
+                        if tab.rawValue == "house" {
+                            Text("Home")
+                                .font(Font.custom("Nunito-Bold", size: 10))
+                                .foregroundColor(selectedTab == tab ? Color("Secondary") : .black)
+                                .padding(.top, 2)
+                        } else if tab.rawValue == "chart.pie" {
+                            Text("Portfolio")
+                                .font(Font.custom("Nunito-Bold", size: 10))
+                                .foregroundColor(selectedTab == tab ? Color("Secondary") : .black)
+                                .padding(.top, 2)
+                        } else if tab.rawValue == "bag" {
+                            Text("Marketplace")
+                                .font(Font.custom("Nunito-Bold", size: 10))
+                                .foregroundColor(selectedTab == tab ? Color("Secondary") : .black)
+                                .padding(.top, 2)
                         }
+                    }
                     
                     Spacer()
                 }
             }
         }
-        .frame(width: nil, height: 60)
+        .frame(width: nil, height: 65)
         .background(.thinMaterial)
         .cornerRadius(10)
         .padding(.horizontal)
+//        .padding(.top, 10)
     }
 }
 
