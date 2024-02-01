7a6f75daaf511af96c0cd411af6b9f85a4acf15e
aym183 <ayman.ali1302@gmail.com>
2024-02-01 16:06:07 +0000

    Integrated Transaction History

---
+++ PARC/View/User/UserTransactionHistory.swift
@@ -0,0 +1,18 @@
+//
+//  UserTransactionHistory.swift
+//  PARC
+//
+//  Created by Ayman Ali on 01/02/2024.
+//
+
+import SwiftUI
+
+struct UserTransactionHistory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserTransactionHistory()
+}
