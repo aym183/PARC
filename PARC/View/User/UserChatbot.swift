559d3ce2872dc7222df55d0119aa847d36c26de3
aym183 <ayman.ali1302@gmail.com>
2024-02-26 11:30:07 +0000

    Added variable sender and receiver messages

--- PARC/View/User/UserChatbot.swift
+++ PARC/View/User/UserChatbot.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct UserChatbot: View {
     @State var text_input = ""
+    @State var test_input = [["type": "Receiver", "data": "Is there anything I can help with to you today with what I do today in"], ["type": "Sender", "data": "Yes of course, I would like some assistance with this"]]
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -24,25 +25,46 @@ struct UserChatbot: View {
                     
                     Spacer()
                     ScrollView(.vertical, showsIndicators: true) {
-                        VStack {
-                            HStack {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 20)
-                                        .fill(Color("Custom_Gray"))
-                                        .opacity(0.2)
-                                    
-                                    Text("Is there anything I can help with to you today with what I do today in ")
-                                        .padding(10)
+                        ForEach(0..<test_input.count, id: \.self) { index in
+                            VStack {
+                                HStack {
+                                    if test_input[index]["type"] == "Receiver" {
+                                        ZStack {
+                                            RoundedRectangle(cornerRadius: 20)
+                                                .fill(Color("Custom_Gray"))
+                                                .opacity(0.2)
+                                            
+                                            Text(test_input[index]["data"]!)
+                                                .padding(5)
+                                            
+                                        }
+                                        .frame(width: geometry.size.width*0.65, height: 70)
+                                        Spacer()
+                                    } else {
+                                        Spacer()
+                                        ZStack {
+                                            RoundedRectangle(cornerRadius: 20)
+                                                .fill(Color("Secondary"))
+                                            
+                                            Text(test_input[index]["data"]!)
+                                                .padding(5)
+                                                .foregroundColor(.white)
+                                            
+                                        }
+                                        .frame(width: geometry.size.width*0.5, height: 70)
+                                    }
+
                                 }
-                                .frame(width: geometry.size.width*0.65, height: 70)
-                                Spacer()
+                                .padding(.vertical, 5)
+                                .multilineTextAlignment(.leading)
+//                                HStack {
+//                                    Spacer()
+//                                    Text("Hei")
+//                                }
                             }
-                            HStack {
-                                Spacer()
-                                Text("Hei")
-                            }
+                            .font(Font.custom("Nunito-SemiBold", size: min(geometry.size.width, geometry.size.height) * 0.035))
+                            .id(index)
                         }
-                        .font(Font.custom("Nunito-SemiBold", size: min(geometry.size.width, geometry.size.height) * 0.035))
                     }
                     .frame(width: max(0, geometry.size.width-40))
                     .padding(.top)
