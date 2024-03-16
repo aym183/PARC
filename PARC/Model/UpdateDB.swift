da4710444b56887c64d6c2d74e0b1ad20fc35bfb
aym183 <ayman.ali1302@gmail.com>
2024-03-16 14:19:48 +0000

    Added comments for UpdateDB

--- PARC/Model/UpdateDB.swift
+++ PARC/Model/UpdateDB.swift
@@ -11,20 +11,19 @@ class UpdateDB: ObservableObject {
     let apiKey = AppConfig.apiKey
     
     
-    /// <#Description#>
+    /// Updates a table's records in the database
     /// - Parameters:
-    ///   - primary_key: <#primary_key description#>
-    ///   - primary_key_value: <#primary_key_value description#>
-    ///   - table: <#table description#>
-    ///   - updated_key: <#updated_key description#>
-    ///   - updated_value: <#updated_value description#>
-    ///   - completion: <#completion description#>
+    ///   - primary_key: Primary key of table to be updated
+    ///   - primary_key_value: Primary key value
+    ///   - table: Name of DB table
+    ///   - updated_key: Updated column name
+    ///   - updated_value: Updated value at column of given row
     func updateTable(primary_key: String, primary_key_value: String, table: String, updated_key: String, updated_value: String, completion: @escaping (String?) -> Void) {
         let apiUrl = URL(string: "https://q3dck5qp1e.execute-api.us-east-1.amazonaws.com/development/payouts?primary_key=\(primary_key)&primary_key_value=\(Int(primary_key_value)!)&table=\(table)&updated_key=\(updated_key)&updated_value=\(updated_value)")!
         var request = URLRequest(url: apiUrl)
         request.httpMethod = "PUT"
         request.addValue(self.apiKey, forHTTPHeaderField: "x-api-key")
-
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data, let responseText = String(data: data, encoding: .utf8) {
                 DispatchQueue.main.async {
@@ -39,20 +38,19 @@ class UpdateDB: ObservableObject {
     }
     
     
-    /// <#Description#>
+    /// Updates a table's records with an integer value in the database
     /// - Parameters:
-    ///   - primary_key: <#primary_key description#>
-    ///   - primary_key_value: <#primary_key_value description#>
-    ///   - table: <#table description#>
-    ///   - updated_key: <#updated_key description#>
-    ///   - updated_value: <#updated_value description#>
-    ///   - completion: <#completion description#>
+    ///   - primary_key: Primary key of table to be updated
+    ///   - primary_key_value: Primary key value
+    ///   - table: Name of DB table
+    ///   - updated_key: Updated column name
+    ///   - updated_value: Updated value at column of given row
     func updateUserTable(primary_key: String, primary_key_value: String, table: String, updated_key: String, updated_value: String, completion: @escaping (String?) -> Void) {
         let apiUrl = URL(string: "https://q3dck5qp1e.execute-api.us-east-1.amazonaws.com/development/payouts?primary_key=\(primary_key)&primary_key_value=\(primary_key_value)&table=\(table)&updated_key=\(updated_key)&updated_value=\(updated_value)")!
         var request = URLRequest(url: apiUrl)
         request.httpMethod = "PUT"
         request.addValue(self.apiKey, forHTTPHeaderField: "x-api-key")
-
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data, let responseText = String(data: data, encoding: .utf8) {
                 DispatchQueue.main.async {
